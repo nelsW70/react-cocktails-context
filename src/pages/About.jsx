013404b03d21a33b080368f8f0a666feabbebbbb
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-18 07:18:14 -0400

    add: Error and About pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section ">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Copper mug mixtape ramps tumblr cardigan food truck, hexagon banh mi
+        activated charcoal. Art party subway tile unicorn 90's sartorial
+        church-key, asymmetrical williamsburg typewriter la croix four dollar
+        toast knausgaard normcore wolf. Celiac pickled leggings kickstarter,
+        pitchfork literally palo santo XOXO messenger bag 3 wolf moon iceland
+        you probably haven't heard of them brooklyn. VHS mustache street art put
+        a bird on it, vegan iceland plaid viral cronut thundercats meggings
+        cornhole.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
